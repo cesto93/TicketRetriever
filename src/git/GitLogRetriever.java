4da208252d8643eb692f916fc445c49b860258e4
cesto93 <cesto93@hotmail.it>
2020-05-25 15:05:04 +0200

    fixed smell

--- src/git/GitLogRetriever.java
+++ src/git/GitLogRetriever.java
@@ -1,82 +1,34 @@
 package git;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
 public class GitLogRetriever {
 	private static final Logger LOGGER = Logger.getLogger(GitLogRetriever.class.getName());
-	private File repo;
-	private File parent;
+	private static final String SHORTDATE = "--date=short";
 	
-	public GitLogRetriever(String repoURL, String repoPath) {
-		this.repo = new File(repoPath);
-		parent = repo.getParentFile();
-		if (!repoExist()) 
-			createRepo(repoURL);
+	private GitDataProvider dataProvider;
+	
+	
+	public GitLogRetriever(GitDataProvider dataProvider) {
+		this.dataProvider = dataProvider;
 	}
 	
-	private boolean repoExist() {
-		if (!repo.exists() || !repo.isDirectory())
-			return false;
-		
-		File git = new File(this.repo.getAbsolutePath() + File.separator + ".git");
-		return (git.exists());
-	}
-	
-	//no checkout
-	private void createRepo(String repoURL) {
-		LOGGER.log(Level.INFO, "creating repo");
-		ProcessBuilder pb = new ProcessBuilder( "git", "clone", repoURL, "-n");
-		pb.directory(parent);
+	public LocalDate getDate(String key) {
+		LocalDate res = null;
 		try {
-			Process p = pb.start(); 
-			if (p.waitFor() != 0 && LOGGER.isLoggable(Level.SEVERE) ) 
-				LOGGER.log(Level.SEVERE, readErrors(p));
-		} catch (IOException | InterruptedException e) {
+			BufferedReader stdInput = dataProvider.getResultStream("git", "log", SHORTDATE, "--pretty=format:\"%cd\"",
+										"--max-count=1", "--grep=" + key);
+			for (String line = stdInput.readLine(); line != null; line = stdInput.readLine()) {
+				res = LocalDate.parse(line.substring(1, line.length() - 1));
+			}
+		} catch (Exception e) {
 			LOGGER.log(Level.SEVERE, e.getMessage(), e);
 			Thread.currentThread().interrupt();
 		}
-	}
-	
-	public LocalDate[] getDates(String[] keys) {
-		ArrayList<LocalDate> res = new ArrayList<>();
-		for (String key : keys) {
-			ProcessBuilder pb = new ProcessBuilder( "git", "log", "--date=short", "--pretty=format:\"%cd\"",
-									"--max-count=1", "--grep=" + key);
-			pb.directory(repo);
-			
-			try {
-				Process p = pb.start();
-				BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
-				String line;
-			
-				while ((line = stdInput.readLine()) != null) {
-					line = line.substring(1, line.length() - 1);
-					res.add(LocalDate.parse(line));
-				}
-				if (p.waitFor() != 0 && LOGGER.isLoggable(Level.SEVERE))
-					LOGGER.log(Level.SEVERE, readErrors(p));
-			} catch (IOException | InterruptedException e) {
-				LOGGER.log(Level.SEVERE, e.getMessage(), e);
-				Thread.currentThread().interrupt();
-			}
-		}
-		return res.toArray(new LocalDate[0]);
-	}
-	
-	private static String readErrors(Process p) throws IOException  {
-		StringBuilder bld = new StringBuilder();
-		String line = "";
-		BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-		while ((line = stdError.readLine()) != null)
-			bld.append(line);
-		return bld.toString();
+		return res;
 	}
 }
--- src/launch/TicketControlChart.java
+++ src/launch/TicketControlChart.java
@@ -1,5 +1,6 @@
 package launch;
 
+import java.io.File;
 import java.time.LocalDate;
 import java.time.YearMonth;
 import java.util.ArrayList;
@@ -9,6 +10,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import git.GitLogRetriever;
+import git.GitRepoHandler;
 import jira.JIRATicketRetriever;
 import model.TicketByMonth;
 import output.CSVExporter;
@@ -21,27 +23,32 @@ public class TicketControlChart {
 	public static void main(String[] args) {	
 		final String resfile = "./ticketByMonth.csv";
 		final String repoUrl = "https://github.com/apache/stdcxx";
-		final String repoPath = "/home/pier/Desktop/uni/isw2/progetto/stdcxx/";
+		final String repoPath = "/home/pier/Desktop/uni/isw2/progetto";
+		final String repoDir = "stdcxx";
 		final String projName = "STDCXX";
 		String[] keys = JIRATicketRetriever.readTicketKeys(projName);
-		createControlChart(keys, repoUrl, repoPath, resfile);
+		createControlChart(keys, repoUrl, new File(repoPath, repoDir), resfile);
 	}
 	
-	public static void createControlChart(String[] keys, String repoUrl, String repoPath, String resfile) {
-		GitLogRetriever retriever = new GitLogRetriever(repoUrl, repoPath);
-		LocalDate[] dates = retriever.getDates(keys);
+	public static void createControlChart(String[] keys, String repoUrl, File repoPath, String resfile) {
+		GitLogRetriever retriever = new GitLogRetriever(new GitRepoHandler(repoUrl, repoPath));
+		LocalDate dates[] = new LocalDate[keys.length];
+		for (int i = 0; i < keys.length; i++) {
+			dates[i] = retriever.getDate(keys[i]);
+		}
 		ArrayList<TicketByMonth> tbm = new ArrayList<>();
 		
 		for (LocalDate date : dates) {
-			tbm.add(new TicketByMonth(YearMonth.from(date), 1));
+			if (date != null)
+				tbm.add(new TicketByMonth(YearMonth.from(date), 1));
 		}
 		
 		Comparator<TicketByMonth> compareByData = (TicketByMonth t1, TicketByMonth t2) -> t1.compareDate(t2);
 		Collections.sort(tbm, compareByData);
 		Reducer.reduce(tbm);
 		Reducer.addEmptyMonths(tbm);
-		LOGGER.log(Level.INFO, tbm.toString());
 		CSVExporter.printCSV(tbm.toArray(new TicketByMonth[0]), resfile);
+		LOGGER.log(Level.INFO, "Done");
 	}
 	
 }
